201433bde2d6ba9b66e5a6818a95416672cb100e
Gustavo Blanco Alfaro <tavoba2015@gmail.com>
2024-12-30 00:48:49 +0000

    Setup devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y vim
+
+git config --global user.email "tavoba2015@gmail.com"
+git config --global user.name "Gustavo Blanco Alfaro"
+git config --global core.editor "vim"
