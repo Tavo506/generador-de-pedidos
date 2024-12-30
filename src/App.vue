4c205af3a0ee6f4602ff8a202d404012cf10649d
Gustavo Blanco Alfaro <tavoba2015@gmail.com>
2024-12-30 10:20:07 -0600

    Config PWA

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import {defineComponent} from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import ReloadPWA from "./components/ReloadPWA.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+    ReloadPWA,
+  },
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ReloadPWA/>
+  <img alt="Vue logo" src="./assets/vue.svg"/>
+  <HelloWorld msg="Hello Vue 3 + Vite"/>
 </template>
+
 
 <style scoped>
 .logo {
@@ -21,9 +26,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
