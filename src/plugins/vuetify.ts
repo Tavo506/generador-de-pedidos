e904f9e8e87fedb4937ee5e47a17225172472d4b
Gustavo Blanco Alfaro <tavoba2015@gmail.com>
2025-05-25 20:52:25 -0600

    Update minor versions, Pinia and Vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,11 +4,9 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, fa } from 'vuetify/iconsets/fa4'
 import 'font-awesome/css/font-awesome.min.css'
-import { VNumberInput } from 'vuetify/labs/VNumberInput'
 
 export default createVuetify({
     components: {
-        VNumberInput,
         ...components
     },
     directives,
